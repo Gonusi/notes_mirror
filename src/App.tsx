95614874c2911f2a8835a11e7f59f60a2ae1529e
kasparas-anusauskas <kasparas.anusauskas@samesystem.com>
2024-10-23 07:09:03 +0300

    Install adobe spectrum lib for UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import Mirror from './components/Mirror'
-import {Box} from "@mui/material";
+import Mirror from "./components/Mirror";
+import { Box } from "@mui/material";
+import StickyNotesBoard from "./components/StickyNotesBoard.tsx";
 
 function App() {
-
   return (
-      <Box sx={{width: '100%', height: '100vh'}}>
-        <Mirror />
-      </Box>
-  )
+    <Box sx={{ width: "100%", height: "100vh" }}>
+      <Mirror />
+      <StickyNotesBoard />
+    </Box>
+  );
 }
 
-export default App
+export default App;
